8bfa4fdd6ee0682672ea9cb85058b4b3d6d75fa2
Jasper Greenberg <jaspergreenberg@gmail.com>
2024-04-03 18:12:30 -0400

    lab for a5

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import cors from "cors";
 
 import Hello from "./Hello.js"
+import Lab5 from "./Lab5.js";
+
 
 const app = express()
+app.use(cors());
+app.use(express.json());
 
 Hello(app)
+Lab5(app);
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
