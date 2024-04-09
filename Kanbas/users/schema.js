f402adef5aa9b6cb203ac9a0e5436b6888b6597a
Jasper Greenberg <jaspergreenberg@gmail.com>
2024-04-09 18:56:15 -0400

    Removed unneeded database specification

--- Kanbas/users/schema.js
+++ Kanbas/users/schema.js
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
-// select a database to use
-const dbName = "kanbas";
-mongoose.connection.useDb(dbName);
-
 const userSchema = new mongoose.Schema(
     {
         username: { type: String, required: true, unique: true },
